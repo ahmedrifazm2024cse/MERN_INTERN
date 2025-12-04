381c8e60ae6adf16b66b929e5dea08a169ebe60e
ahmedrifazm2024cse <ahmedrifaz.m2024cse@sece.ac.in>
2025-12-04 11:17:29 +0530

    ADDING TWO NUMBERS

--- script.js
+++ script.js
@@ -21,3 +21,33 @@ arrow();
     console.log("IIFE executed");
 }
 )();
+
+const add =()=>
+{
+    let a =10;  
+    let b=20;
+    console.log("The Sum is ",a+b);
+}
+add();
+//repeat calls
+add();
+add();
+add();
+
+const addTwoNumbers =(num1,num2)=>
+{
+    console.log("The Sum is ",num1+num2);
+}
+addTwoNumbers(10,20);
+addTwoNumbers();
+
+const addMultipleNumbers =(...numbers)=>
+{
+    let sum =0;     
+    for(let number of numbers)
+    {
+        sum +=number;
+    }           
+    console.log("The Sum is ",sum);
+}
+addMultipleNumbers(10,20,30,40,50);
